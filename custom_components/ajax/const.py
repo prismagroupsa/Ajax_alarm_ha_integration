455548686ecacd95c0dc337473a41a381d65d1ae
Riccardo Margiotta <121126716+riki-90@users.noreply.github.com>
2025-10-09 14:35:07 +0200

    Update const.py

--- custom_components/ajax/const.py
+++ custom_components/ajax/const.py
@@ -1,4 +1,4 @@
 DOMAIN = "ajax"
-DEFAULT_API_URL = "http://ajax.pxy.emea.rmsi.it" #for prod
+DEFAULT_API_URL = "https://ajax.pxy.emea.rmsi.it" #for prod
 # DEFAULT_API_URL = "http://195.36.13.90:8002" #for prod
 # DEFAULT_API_URL = "http://172.19.64.1:8000" #for prod
