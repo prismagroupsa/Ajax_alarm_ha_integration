e716deb625eb71fe7e5d9240127e74ea26fa27b3
Riccardo Margiotta <121126716+riki-90@users.noreply.github.com>
2025-10-20 23:22:43 +0200

    Update const.py

--- custom_components/ajax/const.py
+++ custom_components/ajax/const.py
@@ -1,3 +1,4 @@
 DOMAIN = "ajax"
 
-DEFAULT_API_URL = "http://ajax.proxy.oci.pgsa.cloud" #for prod
+#DEFAULT_API_URL = "http://ajax.proxy.oci.pgsa.cloud" #for prod
+DEFAULT_API_URL = "http://195.36.13.80:1280" #for prod
