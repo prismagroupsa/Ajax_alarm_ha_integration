bf76cb906a7adda1d016816f8633bd6ad10d9959
Riccardo Margiotta <121126716+riki-90@users.noreply.github.com>
2025-11-06 19:04:42 +0100

    Update DEFAULT_API_URL for production environment

--- custom_components/ajax/const.py
+++ custom_components/ajax/const.py
@@ -1,3 +1,4 @@
 DOMAIN = "ajax"
 
-DEFAULT_API_URL = "lbe-ajax-prod-oc1-milan-01.pgsa.cloud"
+#DEFAULT_API_URL = "lbe-ajax-prod-oc1-milan-01.pgsa.cloud"
+DEFAULT_API_URL = "https://ajax.pxy.emea.rmsi.it" #for prod
