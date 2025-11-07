5d91ddb6fa3f660ddcd3f357956b0c67aa049ba1
DShapPG <danielshapran@gmail.com>
2025-11-07 08:02:37 +0000

    domain change

--- custom_components/ajax/const.py
+++ custom_components/ajax/const.py
@@ -1,4 +1,4 @@
 DOMAIN = "ajax"
 
 # DEFAULT_API_URL = "http://172.19.64.1:8000" #for dev
-DEFAULT_API_URL = "https://ajax.pxy.emea.rmsi.it" #for prod
+DEFAULT_API_URL = "http://ajax.proxy.oci.pgsa.cloud" #for prod
